35a276ff03b72bbb34f8843cc740bdaddabe970c
VBERCKER <valentin.bercker@gmail.com>
2024-10-20 09:28:37 +0100

    verc el

--- front/vite.config.js
+++ front/vite.config.js
@@ -8,10 +8,22 @@ export default defineConfig({
   server: {
     fs: {
       allow: [
+        /*
         '/Users/valentinbercker/Downloads/BERCKER_JO_STUDI-main-5/BERCKER_STUDI_DECEMBRE_25/front',
         '/Users/valentinbercker/Desktop/BERCKER_JO_STUDI-main-5/front',
         '/Users/valentinbercker/node_modules/vite/dist/client',
-        '/Users/valentinbercker/Downloads/BERCKER_JO_STUDI-main-5/BERCKER_STUDI_DECEMBRE_25/front/node_modules/vite/dist/client'
+        '/Users/valentinbercker/Downloads/BERCKER_JO_STUDI-main-5/BERCKER_STUDI_DECEMBRE_25/front/node_modules/vite/dist/client',
+       */
+      
+        '/Users/saravaldeztovar/Documents/GitHub/BERCKER_STUDI_DECEMBRE_25/front',
+        '/Users/saravaldeztovar/Downloads/BERCKER_JO_STUDI-main-5/BERCKER_STUDI_DECEMBRE_25/front',
+        '/Users/saravaldeztovar/Desktop/BERCKER_JO_STUDI-main-5/front',
+        '/Users/saravaldeztovar/front/node_modules/vite/dist/client',
+        '/Users/saravaldeztovar/Downloads/BERCKER_JO_STUDI-main-5/BERCKER_STUDI_DECEMBRE_25/front/node_modules/vite/dist/client',
+        '/Users/saravaldeztovar/Desktop/BERCKER_STUDI_DECEMBRE_25-main/front',
+        '/Users/saravaldeztovar/Desktop/BERCKER_STUDI_DECEMBRE_25-main/front/src',
+        '/Users/saravaldeztovar/Desktop/BERCKER_STUDI_DECEMBRE_25-main/front/src/main.jsx',
+        '/Users/saravaldeztovar/Desktop/BERCKER_STUDI_DECEMBRE_25-main/front/style.css'
       ]
     }
   }
